d594d9dacea4707bc9544fe11a1414b79f2b635f
Anish <anish.arvind407@gmail.com>
2024-03-16 16:27:19 +0100

    Login with Azure Ad

--- LoginWithKeycloak/MainWindow.xaml.cs
+++ LoginWithKeycloak/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace LoginWithKeycloak
         private const string ClientId = "WPFAPP";
 
         private const string
-            RedirectUri = "http://localhost:8081/callback"; // Should match the redirect URI configured in Keycloak
+            RedirectUri = "app://localhost:8080/callback"; // Should match the redirect URI configured in Keycloak
 
         private const string Scope = "openid profile email";
         private string _codeVerifier = "";
